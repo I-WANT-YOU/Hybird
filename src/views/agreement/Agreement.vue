a2029c903997c00bdcc8b17bc44165d684fd507c
ansonpro <anson2495@gmail.com>
2019-08-15 00:03:09 +0800

    feat(Agreement): 隐私权协议

--- src/views/agreement/Agreement.vue
+++ src/views/agreement/Agreement.vue
@@ -13,6 +13,18 @@ export default {
     background: #ffffff;
     padding: 15px 20px;
 
+    table {
+      border-collapse: collapse;
+      border-spacing: 0;
+      th,
+      td {
+        padding: 10px;
+        font-size: 12px;
+        line-height: 20px;
+        border: 1px solid #eee;
+      }
+    }
+
     .agm__section {
       font-size: 13px;
       color: #6A707D;
