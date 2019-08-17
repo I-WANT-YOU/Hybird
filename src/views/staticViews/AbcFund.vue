4280108c085f8dd0b8a9648dfb6a3d60f905bad7
lihui-fin-bee <lihui@fin-bee.com>
2019-08-17 22:04:31 +0800

    banner 跳转页面

--- src/views/staticViews/AbcFund.vue
+++ src/views/staticViews/AbcFund.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    <van-image class="image-banner"
-      :src="abcFund"
-    />
+    <header class="header">
+      <div @click="()=>{this.$router.go(-1)}">
+        <svg-icon icon-class="back" class="back-iocn"/>
+      </div>
+      <div><span>一分钟昂你读懂Bgain量化基金</span></div>
+    </header>
+    <van-image class="image-banner" :src="abcFund"/>
   </div>
+
 </template>
 
 <script>
@@ -24,7 +29,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.image-banner{
+  .header{
+    height: 44px;
+    display: flex;
+    align-items: center;
+    font-family: PingFangSC-Regular sans-serif;
+    font-size: 18px;
+    color: #0F3256;
+    letter-spacing: 0;
+    text-align: center;
+    >div:nth-child(1){
+      padding-left: 20px;
+      padding-right: 10px;
+    }
+    >div:nth-child(2){
+      padding-right: 39.5px;
+      display: flex;
+      flex-grow: 1;
+      justify-content: center;
+      align-items: center;
+    }
+    .back-iocn{
+      width: 9.5px;
+      height: 18px;
+    }
+  }
+  .image-banner{
   width: 100%;
-}
+  }
 </style>
--- src/views/staticViews/FundRelease.vue
+++ src/views/staticViews/FundRelease.vue
@@ -1,20 +1,37 @@
 <template>
   <div>
-    <BgainNavBar title = ""/>
-    <div><van-image class="image-banner" :src="fundRelease"/></div>
+    <BgainNavBar title = "第一月度CTA组冠军团队"/>
+    <div>
+      <van-image class="image-banner" :src="fundOne"/>
+      <van-image class="image-banner" :src="fundTwo"/>
+      <van-image class="image-banner" :src="fundThree"/>
+      <van-image class="image-banner" :src="fundFour"/>
+      <van-image class="image-banner" :src="fundFive"/>
+      <van-image class="image-banner" :src="fundSix"/>
+    </div>
   </div>
 </template>
 
 <script>
 import { Image } from 'vant';
 import BgainNavBar from '@component/BgainNavBar.vue';
-import fundRelease from '../../assets/images/staticViews/fund-release.png';
+import fundOne from '../../assets/images/staticViews/fund-one.png';
+import fundTwo from '../../assets/images/staticViews/fund-two.png';
+import fundThree from '../../assets/images/staticViews/fund-three.png';
+import fundFour from '../../assets/images/staticViews/fund-four.png';
+import fundFive from '../../assets/images/staticViews/fund-five.png';
+import fundSix from '../../assets/images/staticViews/fund-six.png';
 
 export default {
   name: 'FundRelease',
   data() {
     return {
-      fundRelease,
+      fundOne,
+      fundTwo,
+      fundThree,
+      fundFour,
+      fundFive,
+      fundSix,
     };
   },
   components: {
