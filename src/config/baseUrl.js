9e002160a2bc0c2c10e2129551d905f7147569dc
lihui-fin-bee <lihui@fin-bee.com>
2019-10-09 17:02:37 +0800

    修改字段顺序，头部高杠杆

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -6,11 +6,11 @@
 
 // const prod = 'http://dev.xjetry.top:8081';
 // // const dev = 'http://dev.xjetry.top:8081';
-// const dev = 'http://dev.xjetry.top:7788';
-// const prod = 'http://dev.xjetry.top:7788';
+const dev = 'http://dev.xjetry.top:7777';
+const prod = 'http://dev.xjetry.top:7777';
 // 线上
-const prod = 'http://dev-hk.xjetry.top:8080';
-const dev = 'http://dev-hk.xjetry.top:8080';
+// const prod = 'http://dev-hk.xjetry.top:8080';
+// const dev = 'http://dev-hk.xjetry.top:8080';
 
 // const prod = 'http://uat.xjetry.top:8091';
 // const dev = 'http://uat.xjetry.top:8091';
